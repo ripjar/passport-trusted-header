4a1c65e0f2182fdcfbae458a0e1c96d1871144ab
Spencer Prost <spencer.prost@live.com>
2019-06-02 12:51:38 -0700

    Update index.js
    Added the info param.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-var util = require("util"),
-    BaseStrategy = require("passport-strategy");
+var util = require('util'),
+  BaseStrategy = require('passport-strategy');
 
-var NAME = "trusted-header";
+var NAME = 'trusted-header';
 
 exports.Strategy = function TrustedHeadersStrategy(options, verify) {
   validateStrategyArgs(options, verify);
@@ -18,33 +18,37 @@ exports.Strategy = function TrustedHeadersStrategy(options, verify) {
 util.inherits(exports.Strategy, BaseStrategy);
 
 function validateStrategyArgs(options, verify) {
-  if(!verify) {
-    throw new Error("Trusted headers strategy requires a verify function");
+  if (!verify) {
+    throw new Error('Trusted headers strategy requires a verify function');
   }
 
-  if(!options.headers) {
-    throw new Error("Trusted headers strategy requires a headers option");
+  if (!options.headers) {
+    throw new Error('Trusted headers strategy requires a headers option');
   }
 }
 
 exports.Strategy.prototype.authenticate = function authenticate(req) {
   var that = this,
-      extractedHeaders = {};
+    extractedHeaders = {};
 
   var foundHeaders = this._headers.every(function(h) {
-    if(req.headers[h]) {
+    if (req.headers[h]) {
       extractedHeaders[h] = req.headers[h];
       return true;
     }
   });
 
-  var verified = function verified(err, user) {
-    if (err) { return that.error(err); }
-    if (!user) { return that.fail(); }
-    that.success(user);
+  var verified = function verified(err, user, info) {
+    if (err) {
+      return that.error(err, info);
+    }
+    if (!user) {
+      return that.fail(info);
+    }
+    that.success(user, info);
   };
 
-  if(foundHeaders) {
+  if (foundHeaders) {
     if (this._passReqToCallback) {
       this._verify(req, extractedHeaders, verified);
     } else {
